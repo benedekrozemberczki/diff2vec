3417d0771bf442c12975ddda436baf1419216678
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2018-05-27 23:11:07 +0100

    Update subgraphcomponents.py

--- src/subgraphcomponents.py
+++ src/subgraphcomponents.py
@@ -1,53 +1,49 @@
-from diffusiontrees import EulerianDiffusionTree, EndPointDiffusionTree
+from diffusiontrees import EulerianDiffuser
 import pandas as pd
 import networkx as nx
-import time
 import random
 import numpy as np
+import time
 
 class SubGraphComponents:
+    """
+    Methods separate the original graph and run diffusion on each node in the subgraphs.
+    """
     
-    def __init__(self, edge_list_path, seeding):
-        
+    def __init__(self, edge_list_path, seeding, vertex_set_cardinality):
+        """
+        Initializing the object with the main parameters.
+        :param edge_list_path: Path to the csv with edges.
+        :param seeding: Random seed.
+        :param vertex_set_cardinality: Number of unique nodes per tree.
+        """
         self.seed = seeding
-        self.start_time = time.time()
+        self.vertex_set_cardinality = vertex_set_cardinality
+        self.read_start_time = time.time()
         self.graph = nx.from_edgelist(pd.read_csv(edge_list_path, index_col = None).values.tolist())
- 
+        self.separate_subcomponents()
+        self.single_feature_generation_run()
+
     def separate_subcomponents(self):
-        
+        """
+        Finding the connected components.
+        """
         self.graph = sorted(nx.connected_component_subgraphs(self.graph), key = len, reverse = True)
+        self.read_time = time.time()-self.read_start_time
         
-    def print_graph_generation_statistics(self):       
-        print("The graph generation at run " + str(self.seed) + " took: " + str(round(time.time() - self.start_time, 3)) + " seconds.\n") 
-        
-    def single_feature_generation_run(self, vertex_set_cardinality, traceback_type):
-        
+    def single_feature_generation_run(self):
+        """
+        Running a round of diffusions and measuring the sequence generation performance.
+        """
         random.seed(self.seed)
-        
-        self.start_time = time.time()
-        
+        self.generation_start_time = time.time()
         self.paths = {}
 
         for sub_graph in self.graph:
- 
-            nodes = sub_graph.nodes()
-            random.shuffle(nodes)
-            
-            current_cardinality = len(nodes)
-            
-            if current_cardinality < vertex_set_cardinality:
-                vertex_set_cardinality = current_cardinality
-            for node in nodes:
-                tree = EulerianDiffusionTree(node)
-                tree.run_diffusion_process(sub_graph, vertex_set_cardinality)
-                path_description = tree.create_path_description(sub_graph)
-                self.paths[node] = map(lambda x: str(x), path_description)
-                
+            current_cardinality = len(sub_graph.nodes())
+            if current_cardinality < self.vertex_set_cardinality:
+                self.vertex_set_cardinality = current_cardinality
+            diffuser = EulerianDiffuser(sub_graph, self.vertex_set_cardinality)
+            self.paths.update(diffuser.diffusions)
         self.paths = self.paths.values()
-                
-    def print_path_generation_statistics(self):
-        print("The sequence generation took: " + str(time.time() - self.start_time))
-        print("Average sequence length is: " + str(np.mean(map(lambda x: len(x), self.paths))))
-        
-    def get_path_descriptions(self):
-        return self.paths
+        self.generation_time = time.time() - self.generation_start_time
