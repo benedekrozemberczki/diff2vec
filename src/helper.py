18bb66bf0c8377e9f5f23c6208b02bffa8ceb521
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2018-12-29 20:47:20 +0100

    Add files via upload

--- src/helper.py
+++ src/helper.py
@@ -13,55 +13,55 @@ def parameter_parser():
 
     parser = argparse.ArgumentParser(description = "Run diffusion2vec.")
 
-    parser.add_argument('--input',
-                        nargs = '?',
-                        default = './data/restaurant_edges.csv',
-	                help = 'Input graph path')
+    parser.add_argument("--input",
+                        nargs = "?",
+                        default = "./data/restaurant_edges.csv",
+	                help = "Input graph path")
 
-    parser.add_argument('--output',
-                        nargs = '?',
-                        default = './output/restaurant.csv',
-	                help = 'Embeddings path')
+    parser.add_argument("--output",
+                        nargs = "?",
+                        default = "./output/restaurant.csv",
+	                help = "Embeddings path")
 
-    parser.add_argument('--model',
-                        nargs = '?',
-                        default = 'pooled',
-	                help = 'Model type.')
+    parser.add_argument("--model",
+                        nargs = "?",
+                        default = "pooled",
+	                help = "Model type.")
 
-    parser.add_argument('--dimensions',
+    parser.add_argument("--dimensions",
                         type = int,
                         default = 128,
-	                help = 'Number of dimensions. Default is 128.')
+	                help = "Number of dimensions. Default is 128.")
 
-    parser.add_argument('--vertex-set-cardinality',
+    parser.add_argument("--vertex-set-cardinality",
                         type = int,
                         default = 40,
-	                help = 'Length of diffusion per source is 2*cardianlity-1. Default is 40.')
+	                help = "Length of diffusion per source is 2*cardianlity-1. Default is 40.")
 
-    parser.add_argument('--num-diffusions',
+    parser.add_argument("--num-diffusions",
                         type = int,
                         default = 10,
-	                help = 'Number of diffusions per source. Default is 10.')
+	                help = "Number of diffusions per source. Default is 10.")
 
-    parser.add_argument('--window-size',
+    parser.add_argument("--window-size",
                         type = int,
                         default = 10,
-                    	help = 'Context size for optimization. Default is 10.')
+                    	help = "Context size for optimization. Default is 10.")
 
-    parser.add_argument('--iter',
+    parser.add_argument("--iter",
                         default = 1,
                         type = int,
-                        help = 'Number of epochs in ASGD. Default is 1.')
+                        help = "Number of epochs in ASGD. Default is 1.")
 
-    parser.add_argument('--workers',
+    parser.add_argument("--workers",
                         type = int,
                         default = 4,
-	                help = 'Number of cores. Default is 4.')
+	                help = "Number of cores. Default is 4.")
 
-    parser.add_argument('--alpha',
+    parser.add_argument("--alpha",
                         type = float,
                         default = 0.025,
-	                help = 'Initial learning rate. Default is 0.025.')
+	                help = "Initial learning rate. Default is 0.025.")
     
     return parser.parse_args()
 
